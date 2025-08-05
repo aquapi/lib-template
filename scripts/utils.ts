02c6cbe8d1e7bec7c64e4724af1effa992ebabc5
Reve <aquaplmc@gmail.com>
2025-08-05 19:19:26 +0700

    Update utils.ts

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,11 +1,12 @@
-import { write, file, $ } from 'bun';
-import { resolve, join } from 'node:path/posix';
+import { join, resolve } from 'node:path/posix';
+import { $, file, write } from 'bun';
 
 export const SCRIPTS = import.meta.dir;
 export const ROOT = resolve(SCRIPTS, '..');
 export const SOURCE = ROOT + '/src';
 export const LIB = ROOT + '/lib';
 export const BENCH = ROOT + '/bench';
+export const EXAMPLES = ROOT + '/examples/src';
 
 export const cp = (from: string, to: string, path: string) =>
   write(join(to, path), file(join(from, path)));
