87447c5f45d2a33a638b1729c22c1057b6d78248
aquapi <aquaplmc@gmail.com>
2026-03-27 19:07:42 +0700

    update

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,3 +1,6 @@
+import { LIB, SOURCE } from './lib/constants.ts';
+import { fmt } from './lib/fmt.ts';
+
 export const test: import('./lib/test.ts').Config = {
   bun: {
     args: {
@@ -44,5 +47,45 @@ export const build: import('./lib/build.ts').Config = {
     },
     mangle: false,
     module: true,
+  },
+};
+
+export const task: import('./task.ts').Config = {
+  tasks: {
+    build: {
+      description: `Build files matching ${build.files.map(fmt.glob).join(', ')} in ${fmt.relativePath(SOURCE)} to ${fmt.relativePath(LIB)}.`,
+      args: {},
+    },
+    test: {
+      description: `Run tests.`,
+      args: {
+        target: {
+          type: 'string[]',
+          description: 'Target tests to run. Run all tests by default.',
+        },
+      },
+    },
+    dev: {
+      description: 'Watch source files and tests.',
+      args: {},
+    },
+    publish: {
+      description: `Publish ${fmt.relativePath(LIB)} to npm.`,
+      args: {
+        otp: {
+          type: '?string',
+          description: 'OTP code to authenticate. Will be prompted if ignored.',
+        },
+      },
+    },
+    'report-size': {
+      description: `Report ${fmt.relativePath(LIB)} files size.`,
+      args: {
+        globs: {
+          type: 'string[]',
+          description: `Files to scan in ${fmt.relativePath(LIB)} to include in the build. Defaults to ${fmt.glob('**/*.js')}.`,
+        },
+      },
+    },
   },
 };
